888d6b2b6bcb56f1e16c7614abe889c19bf1ccca
AfanasyPopov <afanasy.popov@gmail.com>
2015-06-01 01:48:07 +0300

    It work

---
+++ 1CServer/src/afa/server/IOListener.java
@@ -0,0 +1,71 @@
+package afa.server;
+//package by.zti.main;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class IOListener implements Runnable {
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	private boolean isListeining;
+	private ArrayList<Object> readenObject;
+	
+	public IOListener(ObjectOutputStream output, ObjectInputStream input){
+		this.input = input;
+		this.output = output;
+		readenObject = new ArrayList<Object>();
+		new Thread(this).start();
+	}
+	
+	public void sendObject(Object object){
+		try {
+			output.flush();
+			output.writeObject(object);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		isListeining = true;
+		while(isListeining){
+			try {
+				readenObject.add(input.readObject());
+			} catch (ClassNotFoundException e) {
+				//JOptionPane.showMessageDialog(null, e.getMessage());
+			} catch (IOException e) {
+				//JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+		}
+		close();
+	}
+
+	public void close() {
+		isListeining=false;
+		input=null;
+		output=null;
+		readenObject=null;
+	}
+
+	public boolean isListeining() {
+		return isListeining;
+	}
+
+	public void setListeining(boolean isListeining) {
+		this.isListeining = isListeining;
+	}
+
+	public ArrayList<Object> getReadenObject() {
+		return readenObject;
+	}
+
+	public void clearObjects() {
+		readenObject.clear();
+	}
+
+}
---
+++ 1CServer/src/afa/server/User.java
@@ -0,0 +1,51 @@
+package afa.server;
+//package by.zti.main;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+public class User implements Runnable{
+	private Socket connection;
+	private IOListener listener;
+	private ServerConnector connector;
+	private String IP;
+	
+	public User(Socket connection, ServerConnector connector){
+		this.connection = connection;
+		this.connector = connector;
+		this.IP = connection.getInetAddress().getHostAddress();
+		initialise();
+	}
+	
+	public void sendObject(Object object){
+		listener.sendObject(object);
+	}
+	
+	public void initialise(){
+		try {
+			this.listener = new IOListener(new ObjectOutputStream(connection.getOutputStream()), new ObjectInputStream(connection.getInputStream()));
+			new Thread(this).start();
+		} catch (IOException e) {};
+	}
+
+	@Override
+	public void run() {
+		while(connector.isRunning()){
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(!listener.getReadenObject().isEmpty()){
+				connector.addObjects(listener.getReadenObject());
+				listener.clearObjects();
+				System.out.println(connector.getReadenObjects().size());
+			}
+		}
+	}
+	
+}
